92ca82127a15dee926504b5448da61411942eecd
fcu-d0381778 <gujhb93166@gmail.com>
2017-11-05 11:04:07 +0800

    Add Navigation (Issue: UI-010)

---
+++ Web/UI/js/Navigation.js
@@ -0,0 +1,9 @@
+function NavigationShow() {
+    document.getElementById("block").style.visibility = "visible";
+    document.getElementById("navigation_dialog").style.visibility = "visible";
+}
+
+function NavigationBack() {
+    document.getElementById("block").style.visibility = "hidden";
+    document.getElementById("navigation_dialog").style.visibility = "hidden";
+}
