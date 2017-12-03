4e7d2d68b71874d9da7fe8953cac0726ba8b3442
fcu-d0381778 <gujhb93166@gmail.com>
2017-12-03 17:55:45 +0800

    Add about_us page

---
+++ Web/UI/js/about_us.js
@@ -0,0 +1,9 @@
+function aboutDialogShow() {
+    document.getElementById("about_block").style.visibility = "visible";
+    document.getElementById("about_dialog").style.visibility = "visible";
+}
+
+function aboutDialogBack() {
+    document.getElementById("about_block").style.visibility = "hidden";
+    document.getElementById("about_dialog").style.visibility = "hidden";
+}
